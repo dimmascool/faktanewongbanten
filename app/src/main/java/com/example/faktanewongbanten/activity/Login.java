abd5d42c2b09e95218c7a3dc7aa0c41c8aa3fb62
Cilpacito <faizfachrudin22@gmail.com>
2023-01-26 23:46:27 +0700

    Rapihin tampilan login register dll.

--- app/src/main/java/com/example/faktanewongbanten/activity/Login.java
+++ app/src/main/java/com/example/faktanewongbanten/activity/Login.java
@@ -2,15 +2,53 @@ package com.example.faktanewongbanten.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.faktanewongbanten.R;
 
 public class Login extends AppCompatActivity {
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+    private TextView registerpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+        registerpage = findViewById(R.id.registerpage);
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(Login.this, "Please enter a valid username and password", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Perform login using the provided username and password
+                }
+            }
+        });
+
+        registerpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(Login.this, Register.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/faktanewongbanten/activity/Register.java
+++ app/src/main/java/com/example/faktanewongbanten/activity/Register.java
@@ -2,15 +2,55 @@ package com.example.faktanewongbanten.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.faktanewongbanten.R;
 
 public class Register extends AppCompatActivity {
+    private EditText etUsername, etPassword, etConfirmPassword;
+    private Button btnRegister;
+    private TextView loginpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
+        btnRegister = findViewById(R.id.btn_register);
+        loginpage = findViewById(R.id.loginpage);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String confirmPassword = etConfirmPassword.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(Register.this, "Please enter a valid username, password and confirm password", Toast.LENGTH_SHORT).show();
+                } else if (!password.equals(confirmPassword)) {
+                    Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Perform registration using the provided username and password
+                }
+            }
+        });
+
+        loginpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(Register.this, Login.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
