06d64680ecebf10cb4a17d15cc4e45f701141edc
Cilto <faizfachrudin22@gmail.com>
2023-01-29 16:36:00 +0700

    Edit layout in some xml files

--- app/src/main/java/com/example/faktanewongbanten/activity/EditAkun.java
+++ app/src/main/java/com/example/faktanewongbanten/activity/EditAkun.java
@@ -3,14 +3,26 @@ package com.example.faktanewongbanten.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.faktanewongbanten.R;
 
 public class EditAkun extends AppCompatActivity {
 
+    private EditText etUsername, etPassword, etConfirmPassword, etAalamat, etNohp;
+    private Button btnUpdate,btnCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_akun);
+
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
+        etAalamat = findViewById(R.id.et_alamat);
+        etNohp = findViewById(R.id.et_nohp);
+        btnUpdate = findViewById(R.id.btnUpdateAccount);
+        btnCancel = findViewById(R.id.btnCancelUpdate);
     }
 }
