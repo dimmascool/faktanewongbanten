463f1e8fce76369295078da22bc51a2fc0291db7
kenewjr <kenelewatan@gmail.com>
2023-01-31 17:07:50 +0700

    fix button login

--- app/src/main/java/com/example/faktanewongbanten/activity/MenuNonLogin.java
+++ app/src/main/java/com/example/faktanewongbanten/activity/MenuNonLogin.java
@@ -2,15 +2,28 @@ package com.example.faktanewongbanten.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.faktanewongbanten.R;
 
 public class MenuNonLogin extends AppCompatActivity {
-
+    TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_non_login);
+        login = findViewById(R.id.tvUsername);
+
+        login.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuNonLogin.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
